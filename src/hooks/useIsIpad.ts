9e61bbc6d91bc97e72be93bb2efe0f8d5aebc460
rrh1441 <rrh1441@gmail.com>
2024-12-12 16:12:38 -0800

    Remove unnecessary 'window.opera' reference from useIsIpad hook

--- src/hooks/useIsIpad.ts
+++ src/hooks/useIsIpad.ts
@@ -5,7 +5,7 @@ const useIsIpad = (): boolean => {
   const [isIpad, setIsIpad] = useState(false)
 
   useEffect(() => {
-    const ua = navigator.userAgent || navigator.vendor || window.opera?.toString() || ''
+    const ua = navigator.userAgent || navigator.vendor || ''
 
     // Detect iPadOS 13+ which reports as Mac
     const isIpadOS = /Macintosh/.test(ua) && 'ontouchend' in document
